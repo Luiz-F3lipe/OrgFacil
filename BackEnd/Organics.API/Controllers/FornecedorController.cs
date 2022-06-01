ac81161b80299323f8c74a78696030e48b01f891
Luiz F3lipe <luiz.felipe.moraes.lima.1@gmail.com>
2022-05-31 22:01:45 -0300

    add files backend

---
+++ BackEnd/Organics.API/Controllers/FornecedorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Organics.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FornecedorController : ControllerBase
+    {
+        [HttpGet]
+
+        public void teste()
+        {
+
+        }
+    }
+}
